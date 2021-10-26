9aee9edfd888205c080154cdc1e2df73283581ea
angel-zm <54006993+angel-zm@users.noreply.github.com>
2021-10-26 12:07:09 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/practica06/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.practica06;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("CLASE EMPAQUETADA DESDE IDE");
+        System.out.println("La suma es: "+suma(2,2));
+        System.out.println("Nueva linea ");
+    }
+    //retorna suma de dos enteros (a y b)
+   public static int suma(int a,int b){
+   return a+b;
+   } 
+}
